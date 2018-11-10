ed47fdc2d4259b4d59b7a076d8088cf51f7ce669
Yuyang Wang <stevenyuyang@gmail.com>
2018-11-10 12:20:54 -0500

    hooks done

---
+++ src/demoWithHooksInitial.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import './demo.css';
+
+export default class DemoWithHooks extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: 'Hooks',
+            lastName: 'Wang',
+            windowWidth: window.innerWidth,
+        };
+
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+        this.handleLastNameChange = this.handleLastNameChange.bind(this);
+        this.handleResize = this.handleResize.bind(this);
+    }
+
+    componentDidMount() {
+        document.title = `${this.state.firstName}'s Demo`;
+        window.addEventListener('resize', this.handleResize);
+    }
+
+    componentDidUpdate() {
+        document.title = `${this.state.firstName}'s Demo`;
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+
+    handleFirstNameChange(e) {
+        this.setState({
+            firstName: e.target.value
+        });
+    }
+
+    handleLastNameChange(e) {
+        this.setState({
+            lastName: e.target.value
+        });
+    }
+
+    handleResize() {
+        this.setState({
+            windowWidth: window.innerWidth,
+        });
+    }
+
+    render() {
+        return (
+            <div className="inputsContainer isUsinghooks">
+                <TextField
+                    id="first-name"
+                    label="FirstName"
+                    value={this.state.firstName}
+                    onChange={this.handleFirstNameChange}
+                    margin="normal"
+                />
+                <TextField
+                    id="last-name"
+                    label="LastName"
+                    value={this.state.lastName}
+                    onChange={this.handleLastNameChange}
+                    margin="normal"
+                />
+                <TextField
+                    id="windowWidth"
+                    label="WindowWidth"
+                    value={this.state.windowWidth}
+                    margin="normal"
+                />
+            </div>
+        );
+    }
+}
