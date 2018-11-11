7deb07bf6c68d756775a61943fce9955fb60f790
Yuyang Wang <yuyang.wang@jet.com>
2018-11-10 21:50:36 -0500

    update lazySuspense component

--- src/LazySuspense/lazySuspense.js
+++ src/LazySuspense/lazySuspense.js
@@ -1,5 +1,4 @@
-import React, { lazy, Suspense } from 'react';
-import { CircularProgress } from '@material-ui/core';
+import React from 'react';
 
 export default function LazySuspense() {
     return (
