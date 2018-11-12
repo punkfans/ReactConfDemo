37bea56ca5769158249c3b041538c3b682198920
Yuyang Wang <stevenyuyang@gmail.com>
2018-11-11 19:56:12 -0500

    ready to demo background loading

--- src/BackgroundLoading/backgroundLoading.js
+++ src/BackgroundLoading/backgroundLoading.js
@@ -1,19 +1,23 @@
 import React, { lazy, Suspense, useState } from 'react';
 import { Switch, CircularProgress } from '@material-ui/core';
+import { useName } from '../Hooks/hooksFunctionComponent';
+import { TextField } from '@material-ui/core';
+import './backgroundLoading.css';
 
 const SomgBigComponent = lazy(() => {
     return new Promise(resolve => {
-        setTimeout(resolve, 2000);
+        setTimeout(resolve, 5000);
     }).then(() => import('./somgBigComponent'));
 });
 
 export default function BackgroundLoading() {
     const [toggleState, setToggleState] = useState(false);
+    const [inputValue, handleInputValue] = useName('Some Text');
 
     const renderBody = () => {
-        if(!toggleState) {
+        if (!toggleState) {
             return null;
-        }else {
+        } else {
             return (
                 <Suspense fallback={<CircularProgress />}>
                     <SomgBigComponent />
@@ -23,14 +27,21 @@ export default function BackgroundLoading() {
     }
 
     return (
-        <div>
-            { renderBody() }
+        <div className="container">
+            <TextField
+                id="inputField"
+                label="Random Text Field"
+                value={inputValue}
+                onChange={handleInputValue}
+                margin="normal"
+            />
             <Switch
                 checked={toggleState}
                 onChange={() => setToggleState(!toggleState)}
                 value="checkedB"
                 color="primary"
             />
+            { renderBody() }
         </div>
     );
 }
