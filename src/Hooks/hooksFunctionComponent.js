e8c8dcf0645e96f9273e64ace4b2282c3b6c3d09
Yuyang Wang <stevenyuyang@gmail.com>
2018-11-12 20:32:15 -0500

    setup initial stage for function component

--- src/Hooks/hooksFunctionComponent.js
+++ src/Hooks/hooksFunctionComponent.js
@@ -3,64 +3,26 @@ import { TextField } from '@material-ui/core';
 import './demo.css';
 
 export default function HooksFunctionComponent() {
-    const [firstName, handleFirstNameChange] = useName('Haha');
-    const [lastName, handleLastNameChange] = useName('Wang');
-
-    // document title
-    useEffect(() => {
-        document.title = `${firstName}'s Demo`;
-    });
-
-    const windowWidth = useWindowResize();
-
     return (
         <div className="inputsContainer isUsinghooks">
             <TextField
                 id="first-name"
                 label="First Name"
-                value={firstName}
-                onChange={handleFirstNameChange}
+                value={""}
                 margin="normal"
             />
             <TextField
                 id="last-name"
                 label="Last Name"
-                value={lastName}
-                onChange={handleLastNameChange}
+                value={""}
                 margin="normal"
             />
             <TextField
                 id="window-width"
                 label="Window Width"
-                value={windowWidth}
+                value={""}
                 margin="normal"
             />
         </div>
     );
 }
-
-export const useWindowResize = () => {
-    // window resize
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    useEffect(() => {
-        const handleWindowWidthChange = () => {
-            setWindowWidth(window.innerWidth);
-        };
-        window.addEventListener('resize', handleWindowWidthChange);
-
-        return (() => {
-            window.removeEventListener('resize', handleWindowWidthChange);
-        });
-    });
-
-    return windowWidth;
-};
-
-export const useName = (initValue) => {
-    const [name, setName] = useState(initValue);
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
-
-    return [name, handleNameChange];
-};
