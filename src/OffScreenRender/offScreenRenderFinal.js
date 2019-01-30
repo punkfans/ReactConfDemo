18066c5c44e6781f9debc05aea5778b0bab01a2a
Yuyang Wang <stevenyuyang@gmail.com>
2019-01-29 22:13:29 -0500

    add off screen render final back

---
+++ src/OffScreenRender/offScreenRenderFinal.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Switch } from '@material-ui/core';
+import { useName as useResidentialAddress } from '../util/util';
+import { TextField } from '@material-ui/core';
+import './offScreenRender.css';
+import MailingAddressComponent from './mailingAddressComponent';
+
+export default function OffScreenRender() {
+    const [toggleState, setToggleState] = useState(false);
+    const [residentialAddress, handleResidentialAddress] = useResidentialAddress('');
+
+    return (
+        <div className="container">
+            <TextField
+                id="residentialAddress"
+                label="Residential Address"
+                value={residentialAddress}
+                onChange={handleResidentialAddress}
+                margin="normal"
+            />
+            <div>
+                <Switch
+                    checked={toggleState}
+                    onChange={() => setToggleState(!toggleState)}
+                    value="checked"
+                    color="primary"
+                />
+                <span>
+                    My mailing addrss is different from my residential address
+                </span>
+            </div>
+            <div className="mailingAddressContainer">
+                <div hidden={!toggleState}>
+                    <MailingAddressComponent />
+                </div>
+            </div>
+        </div>
+    );
+}
